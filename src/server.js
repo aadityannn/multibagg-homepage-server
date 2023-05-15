67c0ceb2f06904da65f491e0d93647a0403905f0
aadityannn <aaditya.nnn@gmail.com>
2023-05-15 17:53:28 +0530

    Update CORS

--- src/server.js
+++ src/server.js
@@ -11,8 +11,10 @@ const corsOptions = {
         "https://multibagg.com",
         "https://www.multibagg.com",
     ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type,Authorization",
 };
-app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
